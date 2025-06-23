d3f5f33a1a8eecfcc17f489b72d551096c7ac771
Elyes <elyes.bk@orange.fr>
2025-06-23 21:58:33 +0200

    pwa bug reglé

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} 
+/*
 const nextConfig = {};
 
 export default nextConfig;
+*/
+
+import nextPWA from 'next-pwa';
+
+const isDev = process.env.NODE_ENV === 'development'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // ajoute ici d'autres options si nécessaire
+};
+
+export default nextPWA({
+  dest: 'public',
+  disable: isDev, //desactive le pwa en mode developpement
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
+
