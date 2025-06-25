3ecf9d46610d0c1e783948a402acc1e686c1385e
Elyes <elyes.bk@orange.fr>
2025-06-25 19:08:56 +0200

    pwa a jour

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,11 @@ import Nav from '../components/Nav';
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#1E3A5F" />      
+      </head>
       <body>
         <Nav />
         {children}
